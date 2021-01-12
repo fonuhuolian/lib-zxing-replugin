4bc3737db98025fbd9b24f88c8d06fd90557be83
fonuhuolian <44218659+fonuhuolian@users.noreply.github.com>
2021-01-12 09:00:51 +0800

    0.0.1

---
+++ app/src/main/java/com/uuzuche/lib_zxing_replugin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.uuzuche.lib_zxing_replugin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
